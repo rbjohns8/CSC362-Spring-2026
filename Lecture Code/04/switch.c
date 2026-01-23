3f7c14bb862fb332248e3e4922cb174d062916e8
rbjohns4 <rbjohns4@uncg.edu>
2026-01-23 09:01:26 -0500

    code update

--- Lecture Code/04/switch.c
+++ Lecture Code/04/switch.c
@@ -1,14 +1,15 @@
 #include <stdio.h>
 
 int main(void) {
-switch(1) {
-  case 1: /* Goes to this switch */
-    puts("1");
-    break; /* Jumps to the end of the block */
-  case 2: /* Ignores this program */
-    puts("2");
-    break;
-} /* Continues here */
+	switch(1) {
+	  case 1: /* Goes to this switch */
+		puts("1");
+		break; /* Jumps to the end of the block */
+	  case 2: /* Ignores this program */
+		puts("2");
+		break;
+	  default:
+	} /* Continues here */
 }
 
 
