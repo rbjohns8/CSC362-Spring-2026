8a787fe3ca76ce8783a626d1d7518374bbc15077
rbjohns4 <rbjohns4@uncg.edu>
2026-02-27 12:01:02 -0500

    Updating Lecture 12

--- Lecture Code/12/shell_fork_exec.c
+++ Lecture Code/12/shell_fork_exec.c
@@ -2,18 +2,37 @@
 #include <unistd.h>
 #include <sys/types.h>
 #include <sys/wait.h>
+#include <string.h>
 
 int main() {
-    char *cmd[] = {"/bin/ls", NULL};  // Command to execute (ls)
+    char cmd[32];
+    char input[32];
 
     while (1) {
         printf("shell> ");  // Display prompt
         fflush(stdout);  // Ensure prompt is shown
-
+        
+        scanf("%s", input);
+        
+        if ( strcmp(input, "done") == 0 ) {
+          printf("The user entered 'done'.\n");
+          break;
+        }
+        else if ( strcmp(input, "ls" ) == 0 ) {
+          strcpy(cmd, "/bin/ls");
+        }
+        else if ( strcmp(input, "pwd") == 0 ) {
+          strcpy(cmd, "/bin/pwd");
+        }
+        else {
+          printf("Please enter a valid entry.\n");
+          continue;
+        }
+        
         pid_t pid = fork();  // Fork a new process
 
         if (pid == 0) {  // Child process
-            execvp(cmd[0], cmd);  // Replace child with command (ls)
+            execl(cmd, cmd, NULL);  // Replace child with command (ls)
             perror("exec failed");
             _exit(1);
         } else {  // Parent process
